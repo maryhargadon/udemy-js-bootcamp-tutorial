0f3cf81bb5a9fe00362604fd06d901faabae9aa5
mary <maradon853@gmail.com>
2020-09-09 17:33:40 -0500

    finished chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,42 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    //create empty 'chunked' array
+    //create 'index' start at 0
+    //while index is less than array.length
+        //push a slice of length 'size' from 'array' into 'chunked'
+        //add 'size' to 'index'
+
+    const chunked = [];
+    let index = 0;
+    while (index < array.length){
+        chunked.push(array.slice(index, index + size));
+        index += size;
+    }
+    return chunked;
+}
 
 module.exports = chunk;
+
+// function chunk(array, size) {
+//     // create empty array to hold chunks called 'chunked'
+//     // for each element in the 'unchunked' array
+//     // retrieve the last element in 'chunked'
+//     // if last element does not exist, or if its length is equal
+//     //     to chunk size 
+//     // push new chunk into 'chunked' with the current element
+//     // else add the current element into the chunk
+
+//     const chunked = [];
+//     for (let element of array){
+//         const last = chunked[chunked.length - 1];
+//         if (!last || last.length === size){
+//             chunked.push([element]);
+//         }else{
+//             last.push(element);
+//         }
+//     }
+//     return chunked;
+// }
+
