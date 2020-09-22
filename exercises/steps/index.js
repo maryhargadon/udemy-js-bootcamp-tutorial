f5a1f0362d6c593e2f35a81eb4470fe33421cb4a
mary <maradon853@gmail.com>
2020-09-22 17:56:55 -0500

    finished steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,29 +17,51 @@
 //       '### '
 //       '####'
 
-  
-function steps(n) {
-//from 0 to n(iterate through rows)
-//create an empty string, 'stair'
-//from 0 to n (iterate through columns)
-//IF the current column is equal to or less than the current row
-//add a '#' to 'stair'
-//ELSE
-//add a space to 'stair'
-//console log 'stair'
-for (let row = 0; row < n; row++){
-    let stair = '';
-    for (let column = 0; column < n; column++){
-        if (column <= row){
-            stair += '#';
-        }else{
-            stair += " ";
-        }
+
+//RECURSION Version=>
+function steps(n, row = 0, stair = '') {
+    if (n === row){
+        return;
     }
-    console.log(stair);
-}
+    
+    if (n === stair.length) {
+        console.log(stair);
+        return steps(n, row + 1);
+    }
+
+    if (stair.length <= row) {
+        stair += '#';
+    } else {
+        stair += ' ';
+    }
+    steps(n, row, stair);
+
 }
 
 
 
 module.exports = steps;
+
+
+// function steps(n) {
+//     //from 0 to n(iterate through rows)
+//     //create an empty string, 'stair'
+//     //from 0 to n (iterate through columns)
+//     //IF the current column is equal to or less than the current row
+//     //add a '#' to 'stair'
+//     //ELSE
+//     //add a space to 'stair'
+//     //console log 'stair'
+//     for (let row = 0; row < n; row++){
+//         let stair = '';
+//         for (let column = 0; column < n; column++){
+//             if (column <= row){
+//                 stair += '#';
+//             }else{
+//                 stair += " ";
+//             }
+//         }
+//         console.log(stair);
+//     }
+//     }
+    
